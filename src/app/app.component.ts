f6a704b1f8b5889b12e8f9c0c4be7b0c36e6ad5c
oibotero <oscarivanbp@gmail.com>
2019-12-13 17:40:26 -0500

    home Oscar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   //templateUrl: './app.registrarAdministrador.html',
-  templateUrl: './app.registrarLugar_evento.html',
+  //templateUrl: './app.registrarLugar_evento.html',
+  templateUrl: './app.Home.html',
   //templateUrl: './app.eliminarAdministrador.html',
   styleUrls: ['./app.component.scss']
   
